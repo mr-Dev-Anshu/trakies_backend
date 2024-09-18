949ab6bc84dc08a2171636083b45df4e3b336e7b
mr-Dev-Anshu <anshuies31@gmail.com>
2024-09-19 02:36:36 +0530

    update getalltoure route

--- router/Tour.router.js
+++ router/Tour.router.js
@@ -3,6 +3,6 @@ import { createTour, getAllTours, getTourById } from "../controllers/Tour.js";
 import { checkAdminRole } from "../middleware/checkAdminRole.js";
 const router = Router() ; 
 router.route("/create").post( checkAdminRole , createTour)
-router.route("/getAllTours").post(checkAdminRole , getAllTours) 
+router.route("/getAllTours").get(checkAdminRole , getAllTours) 
 router.route("getOneTour").post(checkAdminRole , getTourById) ; 
 export  default router ; 
