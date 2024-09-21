42af753bb35317bd31d23af87d1e808ec924eb29
mr-Dev-Anshu <anshuies31@gmail.com>
2024-09-21 18:38:31 +0530

    fix deployment bug

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./router/user.router.js";
 import { putObject } from "./controllers/s3bucket.js";
-import tourRouter from './router/tour.router.js' ; 
 import memberRouter from './router/member.router.js' ; 
+import { tourRouter } from "./router/Tour.router.js";
 const app = express();
 app.use(express.json());
 dotenv.config({
@@ -14,4 +14,5 @@ app.use("/api/tour", tourRouter);
 app.use("/api/member" , memberRouter) ; 
 app.post("/api/putObject", putObject);
 export { app };
+
 
--- router/Tour.router.js
+++ router/Tour.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { createTour, getAllTours, getTourById } from "../controllers/tour.controller.js";
 import { checkAdminRole } from "../middleware/checkAdminRole.js";
-const router = Router();
-router.route("/create").post(checkAdminRole, createTour);
-router.route("/getAllTours").get(getAllTours);
-router.route("/getTour").get(getTourById);
-export default router;
+const tourRouter  = Router();
+tourRouter.route("/create").post(checkAdminRole, createTour);
+tourRouter.route("/getAllTours").get(getAllTours);
+tourRouter.route("/getTour").get(getTourById);
+export  {tourRouter};   
