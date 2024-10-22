e48619bab25ad65d991367fbf650b0280971471c
mr-Dev-Anshu <anshuies31@gmail.com>
2024-10-23 02:38:00 +0530

    booking status

--- models/Booking.js
+++ models/Booking.js
@@ -1,47 +1,50 @@
 import mongoose from "mongoose";
 
-const bookingSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const bookingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    },
+    tourId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tour",
+      required: true,
+    },
+    status: {
+      type: Number,
+      default: 2,
+    }, 
+    accommodation: {
+      type: Boolean,
+      required: true,
+    },
+    isTrekker: {
+      type: Boolean,
+      required: true,
+    },
   },
-  age: {
-    type: Number,
-    required: true
-  },
-  gender: {
-    type: String,
-    enum: ['Male', 'Female', 'Other'],
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    match: [/.+\@.+\..+/, 'Please fill a valid email address']
-  },
-  tourId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Tour",
-    required: true
-  },
-  status:{
-     type:Boolean , 
-    default:true 
-  }, 
-  accommodation: {
-    type: Boolean,
-    required: true
-  },
-  isTrekker: {
-    type: Boolean,
-    required: true
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields automatically
   }
-}, {
-  timestamps: true // Adds createdAt and updatedAt fields automatically
-});
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 export default Booking;
