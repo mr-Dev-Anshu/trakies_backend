81e094ba6a13ebc8404a47dbc2d3013114830e0d
mr-Dev-Anshu <anshuies31@gmail.com>
2024-11-09 17:52:26 +0530

    accommodation and my tour

--- models/checkedPoints.js
+++ models/checkedPoints.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
 
 const schema = mongoose.Schema({
-    tourId:{
-       type:mongoose.Schema.Types.ObjectId , 
-       ref:"Tour", 
-       required:true   
-    }, 
-     email:{
-         type:String , 
-         required:true 
-     }, 
-     checkPointId:{
-         type:mongoose.Schema.Types.ObjectId  , 
-         ref:"CheckPoint", 
-         required:true
-     }
-}); 
+    tourId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tour",
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    checkPointId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CheckPoint",
+        required: true
+    }
+},
 
-const CheckedPoint = mongoose.model("CheckedPoint" , schema)
+    { timestamps: true });
 
-export default CheckedPoint ; 
+const CheckedPoint = mongoose.model("CheckedPoint", schema)
+
+export default CheckedPoint; 
